63b4bfe77e2b769f512a78738211ff3b63e44081
vbyambaa <73367202+vbyambaa@users.noreply.github.com>
2025-01-08 14:13:24 -0500

    Added files
    I did it !

---
+++ MyMug.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck_2
+{
+    public class MyMug
+    {
+    
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+        public string? Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Color: {Color}, Description: {Description}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace KnowledgeCheck_2;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("How many mugs do you want to add? ");
+        var numberOfMugs = int.Parse(Console.ReadLine());
+
+        var mugList = new List<MyMug>();
+        for (int i = 0; i < numberOfMugs; i++)
+        {
+            Console.WriteLine("Enter the value for mug ");
+
+            var myMug = new MyMug();
+
+            Console.WriteLine("Name: ");
+            myMug.Name = Console.ReadLine();
+
+            Console.WriteLine("Color: ");
+            myMug.Color = Console.ReadLine();
+
+            Console.WriteLine("Description: ");
+            myMug.Description = Console.ReadLine();
+
+
+            mugList.Add(myMug);
+        }
+
+        Console.WriteLine("List of mugs:");
+        foreach (var mug in mugList)
+
+        {
+            Console.WriteLine(mug);
+        }
+    }
+}
+
